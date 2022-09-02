52a76878da33b4b310d63a519b1d7865eb74b64c
BASIC <the_basic_@kookmin.ac.kr>
2022-09-02 16:16:25 +0900

    FEAT: Init Spring
    Add .gitignore
    Add spring project with java

---
+++ src/main/java/harmony/dev/harmonyserver/HarmonyServerApplication.java
@@ -0,0 +1,13 @@
+package harmony.dev.harmonyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/harmony/dev/harmonyserver/HarmonyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package harmony.dev.harmonyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
