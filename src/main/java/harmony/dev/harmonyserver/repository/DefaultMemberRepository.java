a142d0a1512512d8707513ef7f5f13afeb915309
BASIC <the_basic_@kookmin.ac.kr>
2023-01-30 02:21:58 +0900

    Add Slf4j Logger Change return Type for findByOptionalParameters Remove Transactional annotation

--- src/main/java/harmony/dev/harmonyserver/repository/DefaultMemberRepository.java
+++ src/main/java/harmony/dev/harmonyserver/repository/DefaultMemberRepository.java
@@ -1,12 +1,14 @@
 package harmony.dev.harmonyserver.repository;
 
 import harmony.dev.harmonyserver.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
 public class DefaultMemberRepository implements MemberRepository {
 
@@ -23,24 +25,26 @@ public class DefaultMemberRepository implements MemberRepository {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Optional<Member> findByOptionalParameters(String userId, String phoneNumber) {
-        return em.createQuery("SELECT m FROM Member m WHERE 1 = 1"
-                + " AND (:userId is null OR m.userId = :userId)"
-                + " AND (:phoneNumber is null OR m.phoneNumber = :phoneNumber)", Member.class)
+        List<Member> resultList = em.createQuery("SELECT m FROM Member m WHERE 1 = 1"
+                        + " AND (:userId is null OR m.userId = :userId)"
+                        + " AND (:phoneNumber is null OR m.phoneNumber = :phoneNumber)", Member.class)
                 .setParameter("userId", userId)
                 .setParameter("phoneNumber", phoneNumber)
-                .getResultStream().findAny();
+                .getResultList();
+
+        if (resultList.size() >= 2){
+            log.error("ResultList has {} items", resultList.size());
+        }
+        return resultList.stream().findAny();
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Optional<Member> findByUserId(String userId) {
         return findByOptionalParameters(userId, null);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Optional<Member> findByPhoneNumber(String phoneNumber) {
         return findByOptionalParameters(null, phoneNumber);
     }
